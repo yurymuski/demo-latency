6e07b071d06e895ee1d57cea2819690c282d7799
Yury <yury.muski@bdtech.by>
2020-01-24 20:33:25 +0300

    dnscheck bash script

---
+++ dnscheck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# example: 
+# for i in {1..250}; do dig +trace google.com @8.8.8.8 | tail -n2 | grep -i received | awk {'print $8'} >> /tmp/google.com; done && cat /tmp/google.com | Rscript -e 'stat=(scan("stdin")); print(quantile(stat, c(.50,.99)));' && rm /tmp/google.com
+
+for DOMAIN in {'google.com','netflix.com','microsoft.com','cloudflare.com','hoster.by','reg.ru','freedns.afraid.org'}; do
+  for RESOLVER in {'8.8.8.8','1.1.1.1','9.9.9.9','208.67.222.222'}; do
+    for i in {1..250}; do
+      dig +trace ${DOMAIN} @8.8.8.8 | tail -n2 | grep -i received | awk {'print $8'} >> /tmp/${DOMAIN};
+    done;
+    echo -e "\n\n ${DOMAIN} : ${RESOLVER}";
+    cat /tmp/${DOMAIN} | Rscript -e 'stat=(scan("stdin")); print(quantile(stat, c(.50,.99)));';
+    rm /tmp/${DOMAIN};
+  done;
+  echo -e "================== \n";
+done;
