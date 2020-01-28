7fa4bfc58009cb3f7c1dbb33c4be10325dba80fb
Yury <yury.muski@bdtech.by>
2020-01-28 04:00:43 +0300

    TCP check

---
+++ TCP/README.md
@@ -0,0 +1,6 @@
+**User/Server**|**EU response ms**|**Japan response ms**
+:-----:|:-----:|:-----:
+From EU|36.6|332.2
+From Japan|263.9|14.8
+
+Up to 250-300ms improvement using Geo DNS
---
+++ TCP/tcpcheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+#sudo apt-get install -y r-base-core curl
+
+#NOTE: add dns records to /etc/hosts to avoid DNS latency
+
+for DOMAIN in {'demo.jp','demo.eu'}; do
+  for i in {1..10}; do
+    curl -w "%{time_connect}\n" -o /dev/null -s "http://${DOMAIN}" >> /tmp/${DOMAIN};
+  done;
+  echo -e "\n\n ${DOMAIN}";
+  cat /tmp/${DOMAIN} | Rscript -e 'stat=(scan("stdin")); print(quantile(stat, c(.50,.99)));';
+  rm /tmp/${DOMAIN};
+echo -e "================== \n";
+done;
