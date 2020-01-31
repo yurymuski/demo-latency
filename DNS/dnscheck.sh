a114e62cbcf8b0147b1c097947f8cb49edaaac19
Yury <yury.muski@bdtech.by>
2020-01-31 18:11:05 +0300

    added comments

--- DNS/dnscheck.sh
+++ DNS/dnscheck.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-#sudo apt-get install -y r-base-core dnsutils
+#vsudo apt-get install -y r-base-core dnsutils
 
-# example: 
-# for i in {1..250}; do dig +trace google.com @8.8.8.8 | tail -n2 | grep -i received | awk {'print $8'} >> /tmp/google.com; done && cat /tmp/google.com | Rscript -e 'stat=(scan("stdin")); print(quantile(stat, c(.50,.99)));' && rm /tmp/google.com
+# example:
+# dig +trace google.com @8.8.8.8 | tail -n2 | grep -i received
+# for i in {1..1000}; do dig +trace google.com @8.8.8.8 | tail -n2 | grep -i received | awk {'print $8'} >> /tmp/google.com; done && cat /tmp/google.com | Rscript -e 'stat=(scan("stdin")); print(quantile(stat, c(.50,.99)));' && rm /tmp/google.com
 
 for DOMAIN in {'google.com','netflix.com','microsoft.com','cloudflare.com','hoster.by','reg.ru','freedns.afraid.org'}; do
   for RESOLVER in {'8.8.8.8','1.1.1.1','9.9.9.9','208.67.222.222'}; do
-    for i in {1..250}; do
+    for i in {1..1000}; do
       dig +trace ${DOMAIN} @8.8.8.8 | tail -n2 | grep -i received | awk {'print $8'} >> /tmp/${DOMAIN};
     done;
     echo -e "\n\n ${DOMAIN} : ${RESOLVER}";
--- TCP/GeoDns/tcpcheck.sh
+++ TCP/GeoDns/tcpcheck.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-#sudo apt-get install -y r-base-core curl
+# sudo apt-get install -y r-base-core curl
 
-#NOTE: add dns records to /etc/hosts to avoid DNS latency
+# NOTE: add dns records to /etc/hosts to avoid DNS latency
+
+# curl -w "%{time_connect}\n" -o /dev/null -s "http://demo.site"
 
 for DOMAIN in {'demo.jp','demo.eu'}; do
-  for i in {1..10}; do
+  for i in {1..1000}; do
     curl -w "%{time_connect}\n" -o /dev/null -s "http://${DOMAIN}" >> /tmp/${DOMAIN};
   done;
   echo -e "\n\n ${DOMAIN}";
